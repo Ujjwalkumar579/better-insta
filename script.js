32121d553b8ab1e44825e968dac3ae938d7944b6
Ujjwal kumar <69383445+Ujjwalkumar579@users.noreply.github.com>
2022-06-22 16:09:17 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// let dragged = null;
+
+// document.addEventListener("dragstart", event => {
+//   // store a ref. on the dragged elem
+//   dragged = event.target;
+//   console.log("uthaya");
+// });
+
+// document.addEventListener("dragover", event => {
+//   // prevent default to allow drop
+//   event.preventDefault();
+//   console.log("le ke ghum raha ai");
+// });
+
+// document.addEventListener("drop", event => {
+//   // prevent default action (open as link for some elements)
+//   event.preventDefault();
+//   console.log("drop kro");
+//   // move dragged element to the selected drop target
+//   if (event.target.className == "dropzone") {
+//     dragged.parentNode.removeChild(dragged);
+//     event.target.appendChild(dragged);
+//   }
+// });
+
+
